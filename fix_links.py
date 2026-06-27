81939cce5a45ab04f50018743c9abbaa9e7a330b
Euis Samsiah <euissamsiahh@gmail.com>
2026-06-27 23:15:52 +0700

    menambahkan halaman laporan barang masuk admin

--- fix_links.py
+++ fix_links.py
@@ -1,13 +1,18 @@
 import glob
 for f in glob.glob('*.html'):
-    if f == 'Suplier_admin.html':
+    if f == 'laporan_barang_masuk_admin.html':
         continue
     with open(f, 'r', encoding='utf-8') as file:
         content = file.read()
     
-    new_content = content.replace('<a href="#" class="nav-link">Supplier</a>', '<a href="Suplier_admin.html" class="nav-link">Supplier</a>')
+    new_content = content.replace(
+        '<a href="#" class="nav-link">Laporan Barang Masuk</a>',
+        '<a href="laporan_barang_masuk_admin.html" class="nav-link">Laporan Barang Masuk</a>'
+    )
     
     if new_content != content:
         with open(f, 'w', encoding='utf-8') as file:
             file.write(new_content)
         print(f"Updated {f}")
+    else:
+        print(f"No change in {f}")
